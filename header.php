0f704d6a7991ef4bc201a6ca1c37bd1667540d76
MaiconSlavieiro <maiconslavieiro@outlook.com>
2019-05-09 02:09:50 +0000

    add menu dos portais

--- header.php
+++ header.php
@@ -1,34 +1,70 @@
-        <header id="header">
-            <div class="top-header">
-                <div class="top-header-inside container">
-                   <img class="header-arrow" src="assets/images/icons/view-more-icon.svg" width="16px" height="16px"/>
-                   <span class="top-header-portals">Portais Web</span>
-                   <span class="vertical-line"></span>
-                   <div class="top-header-social-medias">
-                        <a href="#"><img class="top-header-facebook-img" src="assets/images/icons/fb-logo.svg" width="22px" height="22px"/></a>
-                        <a href="#"><img class="top-header-instagram-img" src="assets/images/icons/insta-logo.svg" width="22px" height="22px"/></a>
-                   </div>
-                   <span class="vertical-line"></span>
-                   <img class="top-header-phone-img" src="assets/images/icons/phone.png" width="20px" height="20px"/>
-                   <span class="top-header-phone">(54) 2107-7800</span>
-                </div>
-            </div>
-            <div class="main-header">
-                <div class="main-header-inside container">
-                    <div class="logo-barao">
-                    <img src="assets/images/logos/IABRB-logo.svg" alt="Instituto Anglicano Barão do Rio Branco">
-                    </div>
+<header id="header">
+  <div class="top-header">
+    <div class="top-header-inside container">
+      <div class="portalsMenu">
+        <div class="menu" data-toggle="collapse" data-target="#portals">
+          <img class="header-arrow" src="assets/images/icons/view-more-icon.svg" width="16px"height="16px"/>
+          <span class="top-header-portals">Portais Web</span>
+        </div>
+        <div class="collapse" id="portals">
+                <div><a href="#">Portal acadêmico</a></div>
+                <div><a href="#">Portal acadêmico Mobile</a></div>
+                <div><a href="#">Registro de Atendimento</a></div>
+                <div><a href="#">Webmail</a></div>
+                <div><a href="#">Portal de Inscrições</a></div>
+                <div><a href="#">Consulta ao acervo biblioteca</a></div>
+                <div><a href="#">Manuais</a></div>                      
+        </div>
+      </div>
 
-                    <div class="main-header-inside-middle">
-                        <div class="top-menus"></div>
-                        <div class="middle-search"></div>
-                        <div class="bottom-menus"></div>
-                    </div>
+      <span class="vertical-line"></span>
+      <div class="top-header-social-medias">
+        <a href="#"
+          ><img
+            class="top-header-facebook-img"
+            src="assets/images/icons/fb-logo.svg"
+            width="22px"
+            height="22px"
+        /></a>
+        <a href="#"
+          ><img
+            class="top-header-instagram-img"
+            src="assets/images/icons/insta-logo.svg"
+            width="22px"
+            height="22px"
+        /></a>
+      </div>
+      <span class="vertical-line"></span>
+      <img
+        class="top-header-phone-img"
+        src="assets/images/icons/phone.png"
+        width="20px"
+        height="20px"
+      />
+      <span class="top-header-phone">(54) 2107-7800</span>
+    </div>
+  </div>
+  <div class="main-header">
+    <div class="main-header-inside container">
+      <div class="logo-barao">
+        <img
+          src="assets/images/logos/IABRB-logo.svg"
+          alt="Instituto Anglicano Barão do Rio Branco"
+        />
+      </div>
 
-                    <div class="logo-fae">
-                    <img src="assets/images/logos/FAE-logo.svg" alt="Faculdade Anglicana de Erechim">
-                    </div>
-                </div>
-            </div>
-        </header>
+      <div class="main-header-inside-middle">
+        <div class="top-menus"></div>
+        <div class="middle-search"></div>
+        <div class="bottom-menus"></div>
+      </div>
 
+      <div class="logo-fae">
+        <img
+          src="assets/images/logos/FAE-logo.svg"
+          alt="Faculdade Anglicana de Erechim"
+        />
+      </div>
+    </div>
+  </div>
+</header>
