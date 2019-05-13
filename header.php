e7f79d9ba77341791204796507c8e398b0fcf743
HenriqueStaforti <50374060+HenriqueStaforti@users.noreply.github.com>
2019-05-12 23:11:17 -0300

    Update header.php

--- header.php
+++ header.php
@@ -51,12 +51,32 @@
           src="assets/images/logos/IABRB-logo.svg"
           alt="Instituto Anglicano Barão do Rio Branco"
         />
+        <p>Instituto Anglicano Barão Do Rio Branco</p>
       </div>
 
       <div class="main-header-inside-middle">
-        <div class="top-menus"></div>
-        <div class="middle-search"></div>
-        <div class="bottom-menus"></div>
+        <div class="top-menus">
+          <div class="top-menus-content">
+            <span><a href="#">Home</a></span>
+            <span class="vertical-line-top-menu"></span>
+            <span><a href="#">Institucional</a></span>
+            <span class="vertical-line-top-menu"></span>
+            <span><a href="#">Contato</a></span>
+            <span class="vertical-line-top-menu"></span>
+            <span><a href="#">Notícias</a></span>
+          </div>
+        </div>
+        <div class="middle-search">
+          <input type="text" class="header-input-search" id="header-search"/>
+          <button type="submit" class="header-input-search-button"><img src="assets/images/icons/search-icon.svg" width="15px" height="15px"/></button>
+        </div>
+        <nav class="bottom-menus">
+          <div data-toggle="" data-target="#" class="each-bottom-menus">Ensino <img src="assets/images/icons/dropdown-icon.svg" width="9px" height="9px"/></div>
+          <div data-toggle="" data-target="#" class="each-bottom-menus">Eventos <img src="assets/images/icons/dropdown-icon.svg" width="9px" height="9px"/></div>
+          <div data-toggle="" data-target="#" class="each-bottom-menus">Pesquisa e Extensão <img src="assets/images/icons/dropdown-icon.svg" width="9px" height="9px"/></div>
+          <div data-toggle="" data-target="#" class="each-bottom-menus">Secretaria <img src="assets/images/icons/dropdown-icon.svg" width="9px" height="9px"/></div>
+          <div data-toggle="" data-target="#" class="each-bottom-menus">Biblioteca <img src="assets/images/icons/dropdown-icon.svg" width="9px" height="9px"/></div>
+        </nav>
       </div>
 
       <div class="logo-fae">
@@ -64,6 +84,7 @@
           src="assets/images/logos/FAE-logo.svg"
           alt="Faculdade Anglicana de Erechim"
         />
+        <p>Faculdade Anglicana de Erechim</p>
       </div>
     </div>
   </div>
