f73bca56825acff1766d3f78caa377ad087624a8
MaiconSlavieiro <maiconslavieiro@outlook.com>
2019-05-15 00:53:40 +0000

    highlights implementando

--- highlights.php
+++ highlights.php
@@ -1,3 +1,34 @@
-<div id="highlights">
+<section id="highlights" class="container">
+  <div class="title">Destaques</div>
+  
+  <div class="highlights-container">
 
-</div>
+    <div class="box">
+      <img src="assets\images\highlights\highlight-1.png" alt="">
+      <div class="post-title">
+        <p>Análise de Sistemas da FAE desenvolverá mais de 20 projetos em benefício da comunidade</p>
+      </div>
+    </div>
+
+    <div class="box">
+      <img src="assets\images\highlights\highlight-2.png" alt="">
+      <div class="post-title">
+        <p>Ensino Médio do Barão visita Usina Hidrelétrica Itá</p>
+      </div>
+    </div>
+
+    <div class="box">
+      <img src="assets\images\highlights\highlight-3.png" alt="">
+      <div class="post-title">
+        <p>Mais de 15 cursos de Pós-Graduação com inscrições abertas na FAE</p>
+      </div>
+    </div>
+   
+  </div>
+
+  <button class="showMore" href="#"> 
+    Ver mais notícias
+    <i class="fas fa-angle-double-right"></i>
+  </button>
+
+</section>
--- index.php
+++ index.php
@@ -8,6 +8,8 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet/less" type="text/css" href="assets/less/styles.less"/>
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
 
   <script src="assets/js/jquery.min.js"></script>
   <script src="assets/js/popper.min.js"></script>
