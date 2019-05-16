9f6d9b9ec4840d722e354e45ba1d45c1706b0f01
MaiconSlavieiro <maiconslavieiro@outlook.com>
2019-05-16 13:12:14 -0300

    events section update

--- schedule.php
+++ schedule.php
@@ -1,5 +1,66 @@
 <section class="schedule">
   <div class="container">
-    
+  
+    <div class="title">AGENDA</div>
+
+    <div class="schedule-container">
+
+      <div class="event-box">
+
+        <div class="data-box">
+          <div class="data">22 de Abril</div>
+        </div>
+
+        <div class="divider"></div>
+
+        <div class="day-events-box">
+
+        <div class="event">
+
+          <div class="event-title">Morbi maximus, purus at lobortis dictum</div>
+          <div class="event-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus, purus at lobortis</div>
+          <div class="event-local">Local: Auditorio IABRB | FAE</div>
+          <div class="event-hour">Horário: 19:30</div>
+
+        </div>  
+
+        <div class="event">
+
+          <div class="event-title">Morbi maximus, purus at lobortis dictum</div>
+          <div class="event-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus, purus at lobortis</div>
+          <div class="event-local">Local: Auditorio IABRB | FAE</div>
+          <div class="event-hour">Horário: 19:30</div>
+
+        </div>  
+
+        </div>
+
+      </div>
+
+      <div class="event-box">
+
+        <div class="data-box">
+          <div class="data">28 de Maio</div>
+        </div>
+
+        <div class="divider"></div>
+
+        <div class="day-events-box">
+
+          <div class="event">
+
+            <div class="event-title">Morbi maximus, purus at lobortis dictum</div>
+            <div class="event-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus, purus at lobortis</div>
+            <div class="event-local">Local: Auditorio IABRB | FAE</div>
+            <div class="event-hour">Horário: 19:30</div>
+
+          </div>          
+
+        </div>
+
+      </div>
+      
+    </div>
+
   </div>
 </section>
